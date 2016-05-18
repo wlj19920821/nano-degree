8d6bf526bcf394b7c785bd5d31cc2ba0d6bc996c
wlj19920821 <wlj0821@foxmail.com>
2016-05-18 11:30:17 -0400

    make changes according to reviewer's suggestion.

--- app/src/main/java/com/example/android/mynanodegreeapps/MainActivity.java
+++ app/src/main/java/com/example/android/mynanodegreeapps/MainActivity.java
@@ -1,55 +1,38 @@
 package com.example.android.mynanodegreeapps;
 
 import android.content.Context;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
-
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
     ;
-    public void sendMessage(View view) {
-        CharSequence text = "This button will launch my capstone app!";
+    public void showToastMessage(String text) {
         int duration = Toast.LENGTH_SHORT;
         Context context = getApplicationContext();
         Toast toast = Toast.makeText(context, text, duration);
         toast.show();
     }
     public void sendMessage1(View view) {
-        CharSequence text = "This button will launch my spotify streamer app!";
-        int duration = Toast.LENGTH_SHORT;
-        Context context = getApplicationContext();
-        Toast toast = Toast.makeText(context, text, duration);
-        toast.show();
+        showToastMessage("This button will launch my spotify streamer app!");
     }
     public void sendMessage2(View view) {
-        CharSequence text = "This button will launch my scores app!";
-        int duration = Toast.LENGTH_SHORT;
-        Context context = getApplicationContext();
-        Toast toast = Toast.makeText(context, text, duration);
-        toast.show();
+        showToastMessage("This button will launch my scores app!");
     }
     public void sendMessage3(View view) {
-        CharSequence text = "This button will launch my library app!";
-        int duration = Toast.LENGTH_SHORT;
-        Context context = getApplicationContext();
-        Toast toast = Toast.makeText(context, text, duration);
-        toast.show();
+        showToastMessage("This button will launch my library app!");
     }
     public void sendMessage4(View view) {
-        CharSequence text = "This button will launch my build it bigger app!";
-        int duration = Toast.LENGTH_SHORT;
-        Context context = getApplicationContext();
-        Toast toast = Toast.makeText(context, text, duration);
-        toast.show();
+        showToastMessage("This button will launch my build it bigger app!");
     }
     public void sendMessage5(View view) {
-        CharSequence text = "This button will launch my xyz reader app!";
-        int duration = Toast.LENGTH_SHORT;
-        Context context = getApplicationContext();
-        Toast toast = Toast.makeText(context, text, duration);
-        toast.show();
+       showToastMessage("This button will launch my xyz reader app!");
     }
 
 }
